87defa795201d6075bdeec141d9d4d6c0fcf25ea
Brett Chalupa <brettchalupa@gmail.com>
2021-08-24 08:37:32 -0400

    Set default font to 12 since anything smaller is blurry

--- source/BaseState.hx
+++ source/BaseState.hx
@@ -8,7 +8,7 @@ class BaseState extends FlxState
 {
 	var input:InputManager = new InputManager();
 
-	function addText(text:String, size:Int = 8, x:Float = 0, y:Float = 0, color:Int = Color.WHITE):FlxText
+	function addText(text:String, size:Int = 12, x:Float = 0, y:Float = 0, color:Int = Color.WHITE):FlxText
 	{
 		var text = new FlxText(x, y, 0, text, size);
 		text.font = "Fairfax";

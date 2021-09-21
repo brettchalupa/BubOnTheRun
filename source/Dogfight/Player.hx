371f42e4c22b25f61135f22ab717a18720541e0d
Brett Chalupa <brettchalupa@gmail.com>
2021-09-20 20:01:00 -0400

    Clone Spacevania to Dogfight
    This takes the very early Spacevania and clones it to Dogfight to make
    the dogfighting flying game since that's a fine starting place.

---
+++ source/Dogfight/Player.hx
@@ -0,0 +1,139 @@
+package dogfight;
+
+import InputManager.Action;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.group.FlxGroup;
+import flixel.math.FlxAngle;
+import flixel.math.FlxPoint;
+import flixel.math.FlxVelocity;
+import flixel.math.FlxVelocity;
+import flixel.util.FlxColor;
+import flixel.util.FlxSort;
+
+class Player extends FlxSprite
+{
+	public var bullets:FlxTypedGroup<FlxSprite>;
+
+	final MAX_BASE_VEL = 70;
+	final MAX_THRUST_VEL = 160;
+	final THRUST_ACCEL = 120;
+	final BASE_ACCEL = 50;
+	final BRAKE_ACCEL = -40;
+	final ANG_CHANGE_PER_SEC = 3;
+	final MAX_BULLETS = 40;
+	final SHOT_COOLDOWN = 0.25;
+	var lastAcceleration = 0.0;
+	var input:InputManager;
+	var timeSinceLastShot:Float = 0.0;
+	var thrust:Float = 0.0;
+	final MAX_ANGULAR = 120;
+	final ANGULAR_DRAG = 420;
+	final DRAG = 3;
+
+	public function new(_input:InputManager)
+	{
+		super();
+		input = _input;
+		loadGraphic("assets/images/dogfight/player.png", 16);
+		screenCenter();
+		drag.set(DRAG, DRAG);
+		setSize(6, 6);
+		centerOffsets();
+		solid = true;
+
+		maxAngular = MAX_ANGULAR;
+		angularDrag = ANGULAR_DRAG;
+		drag.x = DRAG;
+
+		bullets = new FlxTypedGroup<FlxSprite>(MAX_BULLETS);
+
+		for (i in 0...MAX_BULLETS)
+		{
+			var bullet = new FlxSprite().loadGraphic("assets/images/dogfight/bullet.png", 16);
+			bullet.setSize(1, 2);
+			bullet.kill();
+			bullets.add(bullet);
+		}
+
+		#if debug
+		FlxG.watch.add(this, "angle");
+		FlxG.watch.add(this, "velocity");
+		FlxG.watch.add(this, "acceleration");
+		#end
+	}
+
+	override public function update(elapsed:Float):Void
+	{
+		handleInput(elapsed);
+
+		bullets.forEachAlive(function(b)
+		{
+			if (!b.inWorldBounds())
+			{
+				b.kill();
+			}
+		});
+
+		super.update(elapsed);
+	}
+
+	public function handleInput(elapsed:Float)
+	{
+		handleSteering(elapsed);
+
+		if (input.pressed(Action.CONFIRM) && timeSinceLastShot >= SHOT_COOLDOWN)
+		{
+			timeSinceLastShot = 0.0;
+			FlxG.sound.play(AssetPaths.click__wav);
+			fireBullet();
+		}
+		else
+		{
+			timeSinceLastShot += elapsed;
+		}
+	}
+
+	function fireBullet()
+	{
+		var bullet = bullets.recycle(FlxSprite);
+		bullet.setPosition(getMidpoint().x, getMidpoint().y);
+		bullet.angle = angle;
+		bullet.velocity.set(0, -100);
+		bullet.velocity.rotate(FlxPoint.weak(0, 0), bullet.angle);
+	}
+
+	function handleSteering(elapsed:Float)
+	{
+		if (input.pressed(Action.LEFT))
+		{
+			angularAcceleration = -angularDrag;
+		}
+		else if (input.pressed(Action.RIGHT))
+		{
+			angularAcceleration = angularDrag;
+		}
+		else
+		{
+			angularAcceleration = 0;
+		}
+
+		var _up:Bool = false;
+		var _down:Bool = false;
+
+		if (input.pressed(Action.UP))
+		{
+			color = Color.PINK;
+			_up = true;
+		}
+		else
+		{
+			color = Color.WHITE;
+		}
+
+		thrust = FlxVelocity.computeVelocity(thrust, (_up ? 90 : 0), drag.x, 60, elapsed);
+		velocity.set(0, -thrust);
+		velocity.rotate(FlxPoint.weak(0, 0), angle);
+	}
+}
--- source/DogfightState.hx
+++ source/DogfightState.hx
@@ -1,188 +1,44 @@
 package;
 
-import Color;
-import InputManager.Action;
+import dogfight.Player;
+import flixel.FlxCamera;
 import flixel.FlxG;
-import flixel.FlxSprite;
-import flixel.group.FlxGroup.FlxTypedGroup;
-import flixel.text.FlxText;
-import flixel.util.FlxAxes;
-
-using StringTools;
+import flixel.addons.display.FlxBackdrop;
 
 class DogfightState extends GameState
 {
-	override public function gameType():GameType
-	{
-		return GameType.DOGFIGHT;
-	}
-
-	var player:FlxSprite;
-	var bullets:FlxTypedGroup<FlxSprite>;
-	var enemies:FlxTypedGroup<FlxSprite>;
-	var score:Int = 0;
-	var scoreText:FlxText;
-	var gameOver:Bool = false;
-	var difficultyLevel = 1;
-
-	override public function create()
-	{
-		super.create();
-		FlxG.worldBounds.set(0, 0, FlxG.width, FlxG.height);
-
-		player = new FlxSprite();
-		player.makeGraphic(8, 8, Color.PINK);
-		player.screenCenter();
-		player.y = FlxG.height - 32;
-		add(player);
-
-		bullets = new FlxTypedGroup<FlxSprite>();
-		enemies = new FlxTypedGroup<FlxSprite>();
-
-		spawnEnemies();
-		spawnEnemies();
-		spawnEnemies();
-		spawnEnemies();
-		spawnEnemies();
-
-		add(enemies);
-
-		scoreText = addText('Score: 000000', 12, 6, 6);
-		scoreText.alignment = "right";
-		scoreText.x = FlxG.width - scoreText.width - 12;
-		updateScore(0);
-	}
-
 	override function slug():String
 	{
 		return "dogfight";
 	}
 
+	override public function gameType():GameType
+	{
+		return GameType.DOGFIGHT;
+	}
+
+	var player:Player;
+	var isGameOver:Bool = false;
+	var totalElapsedTime:Float;
+
+	override public function create()
+	{
+		super.create();
+
+		FlxG.cameras.bgColor = Color.BLACK;
+
+		add(new FlxBackdrop("assets/images/dogfight/bg.png"));
+
+		player = new Player(input);
+		add(player.bullets);
+		add(player);
+
+		FlxG.camera.follow(player, FlxCameraFollowStyle.LOCKON);
+		FlxG.worldBounds.set(0, 0, FlxG.width, FlxG.height);
+	}
+
 	override public function update(elapsed:Float)
 	{
-		if (gameOver)
-		{
-			if (input.pressed(Action.CONFIRM))
-			{
-				FlxG.resetState();
-			}
-		}
-		else
-		{
-			if (input.pressed(Action.LEFT))
-			{
-				player.x -= 2;
-			}
-			if (input.pressed(Action.RIGHT))
-			{
-				player.x += 2;
-			}
-			if (input.justPressed(Action.CONFIRM))
-			{
-				bullets.add(shootBullet(elapsed));
-			}
-
-			if (player.x < 0)
-			{
-				player.x = 0;
-			}
-
-			if (player.x + player.width > FlxG.width)
-			{
-				player.x = FlxG.width - player.width;
-			}
-
-			for (bullet in bullets)
-			{
-				if (bullet.y + bullet.height < 0)
-				{
-					bullets.remove(bullet);
-					bullet.kill();
-					bullet.destroy();
-				}
-			}
-
-			for (enemy in enemies)
-			{
-				if (enemy.y >= FlxG.height)
-				{
-					enemies.remove(enemy);
-					enemy.kill();
-					enemy.destroy();
-					spawnEnemies();
-				}
-			}
-
-			FlxG.overlap(bullets, enemies, onBulletHitEnemy);
-			FlxG.overlap(enemies, player, onEnemyHitPlayer);
-		}
-
 		super.update(elapsed);
 	}
-
-	function onBulletHitEnemy(bullet:FlxSprite, enemy:FlxSprite)
-	{
-		bullet.kill();
-		bullets.remove(bullet);
-		enemy.kill();
-		enemies.remove(enemy);
-		enemy.destroy();
-		bullet.destroy();
-		updateScore(100);
-		spawnEnemies();
-	}
-
-	function onEnemyHitPlayer(enemy:FlxSprite, player:FlxSprite)
-	{
-		player.kill();
-		enemy.kill();
-		enemies.remove(enemy);
-		doGameOver();
-	}
-
-	function doGameOver()
-	{
-		gameOver = true;
-		FlxG.camera.shake(0.02, 0.25, function()
-		{
-			addText("Game Over", 24, 0, 0).screenCenter();
-			addText("Press ACTION to restart", 12, 0, FlxG.height - 32).screenCenter(FlxAxes.X);
-		});
-	}
-
-	function spawnEnemies()
-	{
-		for (_ in 0...difficultyLevel)
-		{
-			var enemy = new FlxSprite();
-			enemy.makeGraphic(6, 6, Color.ORANGE);
-			enemy.y = FlxG.random.float(-10, -100);
-			enemy.x = FlxG.random.float(0 + enemy.width, FlxG.width - enemy.width);
-			enemy.velocity.y = FlxG.random.float(20, 50);
-			enemies.add(enemy);
-		}
-	}
-
-	function shootBullet(elapsed:Float):FlxSprite
-	{
-		var bullet = new FlxSprite();
-		bullet.makeGraphic(2, 2, Color.RED);
-		bullet.x = player.x + player.width / 2 - bullet.width / 2;
-		bullet.y = player.y - bullet.height;
-		bullet.velocity.y = -100;
-		add(bullet);
-		FlxG.sound.play(AssetPaths.click__wav);
-		return bullet;
-	}
-
-	function updateScore(amount)
-	{
-		score += amount;
-		if (score == 1000)
-		{
-			difficultyLevel++;
-		}
-		var formattedScore = Std.string(score).lpad("0", 6);
-		scoreText.text = 'Score: $formattedScore';
-	};
 }
